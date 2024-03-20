98913ac409e42e8dc8679a2a6f8f2cece69314ea
Ivan puig <ivan@bpuig.com>
2024-03-20 18:32:32 +0000

    Revert separation of content and dev

--- pages/pages.go
+++ pages/pages.go
@@ -1,12 +1,20 @@
 package pages
 
 type BlogPost = struct {
+    Title string
+    abstract string
 }
 
 type HomeData = struct {
     Title string
+    About string
     Styles []string
 }
 
 type NavigationBar = struct {
+}
+
+type Footer = struct {
+    Title string
+    Links []string
 }
--- routes/routes.go
+++ routes/routes.go
@@ -2,9 +2,9 @@ package routes
 
 import (
 	"blog/pages"
+	"fmt"
 	"html/template"
 	"net/http"
-        "fmt"
 )
 
 func Routes() *http.ServeMux {
@@ -12,13 +12,19 @@ func Routes() *http.ServeMux {
     mux.HandleFunc("/", HomePage)
     mux.HandleFunc("/test", RandomCrap("<h2> THIS IS A TEST!</h2>"))
     mux.HandleFunc("/secret", RandomCrap("sjsldfkjlk[j4klj2lkjdfkljslakjflj"))
+
+    /* Handle resources*/
+    resources := http.FileServer(http.Dir("./assets"))
+    mux.Handle("/resources/", http.StripPrefix("/resources/", resources))
+
     return mux
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("index.html"))
-        data := pages.HomeData { Title: "Home", Styles: []string { "assets/css/base.css"} }
-        tmpl.Execute(w, data)
+
+    tmpl := template.Must(template.ParseFiles("index.html"))
+    data := pages.HomeData { Title: "Home", Styles: []string {"resources/css/base.css"} }
+    tmpl.Execute(w, data)
 }
 
 func RandomCrap(text string) func(http.ResponseWriter, *http.Request) {
